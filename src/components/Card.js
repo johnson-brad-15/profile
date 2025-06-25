d0a2748cf5afbeef9c8cdd521a937719debefb5d
Brad Johnson <johnson.brad15@gmail.com>
2025-06-25 14:57:24 -0500

    Updated text

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,21 +3,17 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 import React from "react";
 
-const Card = ({ title, description, content, seemore }) => {
-  // Implement the UI for the Card component according to the instructions.
-  // You should be able to implement the component with the elements imported above.
-  // Feel free to import other UI components from Chakra UI if you wish to.
-  console.log("Desc: " ,description);
+const Card = ({ title, description, content, seemore, fonti_size_desc='xs' }) => {
   return (
     <div className='card container'>
       {/* <Image className='card src' src={src}/> */}
       <div className='card content_container'>{content}</div>
       <VStack className='card verbiage'>
         <Text className='card title' fontSize='lg' fontWeight='bold'>{title}</Text>
-        <Text className='card description' fontSize='sm' fontWeight='normal'>{description}</Text>
+        <Text className='card description' whiteSpace="pre-line" fontSize={fonti_size_desc} fontWeight='normal'>{`${description}`}</Text>
         <Flex className='seemore_flexrow' direction='row' justify='flex-start' align='flex-end'>
           <Link className='seemore' fontSize='xs' fontWeight='normal' href={seemore}>
-            See more <FontAwesomeIcon icon={faArrowRight} size="1x" />
+            Code <FontAwesomeIcon icon={faArrowRight} size="1x" />
           </Link>
         </Flex>
       </VStack>
